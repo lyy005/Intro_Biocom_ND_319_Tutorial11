79584706743d1ab51dbb57331fd0c00ea9d7b5e8
Phil McCown <pjlmac@gmail.com>
2017-11-10 13:48:29 -0500

    Problem 1 is completely done

--- Exercise-Files/Ex11Q1-1.sh
+++ Exercise-Files/Ex11Q1-1.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/ bash
+
 for i in Problem1/*.ref
 	do 
 	../../.././muscle3.8.31_i86win32 -in $i -out $i.aln
 	../../../hmmer-3.1b2-cygwin64/binaries/./hmmbuild $i.hmm $i.aln
-	done
-for i in Problem1/*.fasta
-	do 
-	../../../hmmer-3.1b2-cygwin64/binaries/./hmmsearch sigma.hmm i 
-> sigma.hmms
-	../../../hmmer-3.1b2-cygwin64/binaries/./hmmsearch sporecoat.hmm i 
-> sporecoat.hmms
-	../../../hmmer-3.1b2-cygwin64/binaries/./hmmsearch transporter.hmm 
-i > transporter.hmms
-	done
+	done 
+cat Problem1/*.fasta >> all-files.fasta
+../../../hmmer-3.1b2-cygwin64/binaries/./hmmsearch Problem1/sigma.ref.hmm all-files.fasta > sigma.hmms
+../../../hmmer-3.1b2-cygwin64/binaries/./hmmsearch Problem1/sporecoat.ref.hmm all-files.fasta > sporecoat.hmms
+../../../hmmer-3.1b2-cygwin64/binaries/./hmmsearch Problem1/transporter.ref.hmm all-files.fasta > transporter.hmms
 	
  
