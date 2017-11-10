eaf1101b91e94f4d748f6efe36dc67ffdc456fd4
Katherine <bookaholic214@gmail.com>
2017-11-10 10:47:53 -0500

    Made Python script, added pseudocode for Q1

---
+++ Exercise11_files/exercise11.py
@@ -0,0 +1,16 @@
+###Question 1###
+
+for i in *ref
+do
+    muscle -in 4i -out $i.aln
+    hmmbuild ________
+done
+
+for i in *fasta
+do
+    hmmsearch #model1
+    hmmsearch #model2
+    hmmsearch #model3
+done
+
+####Question 2####
