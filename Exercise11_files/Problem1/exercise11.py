0d81defcef97ca9b2e4f94875beaf9c4eb8273da
Mati Nemera <mnemera@nd.edu>
2017-11-13 16:41:33 -0500

    changed python for problem 1 a little

--- Exercise11_files/Problem1/exercise11.py
+++ Exercise11_files/Problem1/exercise11.py
@@ -3,12 +3,12 @@
 for i in *ref
 do
     ../../../../muscle.exe -in $i -out $i.aln
-    ../../../../hmmer-3.1b2-cygwin64/binaries/hmmbuild $i.hmm $i.align
+    ../../../../hmmer-3.1b2-cygwin64/binaries/hmmbuild $i.hmm $i.aln
 done
 
 for i in *fasta
 do
-    ../../../../hmmer-3.1b2-cygwin64/binaries/hmmsearch --tblout sigma.hits sigma.hmm $i
-    ../../../../hmmer-3.1b2-cygwin64/binaries/hmmsearch --tblout sporecoat.hits sporecoat.hmm $i
-    ../../../../hmmer-3.1b2-cygwin64/binaries/hmmsearch --tblout transporter.hits transporter.hmm $i
+    ../../../../hmmer-3.1b2-cygwin64/binaries/hmmsearch --tblout $i.sigma.hits sigma.ref.hmm $i
+    ../../../../hmmer-3.1b2-cygwin64/binaries/hmmsearch --tblout $i.sporecoat.hits sporecoat.ref.hmm $i
+    ../../../../hmmer-3.1b2-cygwin64/binaries/hmmsearch --tblout $i.transporter.hits transporter.ref.hmm $i
 done
