e41ba1181597e0a755d3cb575cae77d4157ae06b
kkilgoreND <31992787+kkilgoreND@users.noreply.github.com>
2017-11-14 10:57:55 -0500

    working part 2

--- Exercise-Files/Ex11Q2.py
+++ Exercise-Files/Ex11Q2.py
@@ -1,21 +1,35 @@
 import numpy
 import pandas
 import re
+
 infile="motifsort.fasta"
 out1="motif1.fasta"
 out2="motif2.fasta"
 null="null.fasta"
 
+#define the patterns
+Pat1="AKKPRVZE"
+Pat2="AAQWWRNYGG"
+
 for line in infile:
     line=line.strip()
-    if ">":
+    if (line[0] == ">"):
         seqid=line
-    elif "AKKPRVZE":
-        print seqid
-        print line
-    elif "AAQWWRNYGG":
-        print seqid
-        print line
-    else:
-        print seqid
-        print line
+    else (">" not in line):
+        if (re.search(Pat1, line):
+            out1.write(seqid + "\n")
+            out1.write(line + "\n")
+        elif (re.search(Pat2, line):
+              out2.write(seqid + "\n")
+              out2.write(line + "\n")
+        else:
+              null.write(seqid + "\n")
+              null.write(line + "\n")
+              
+#Close files
+infile.close()
+out1.close()
+out2.close()
+null.close()
+              
+        
