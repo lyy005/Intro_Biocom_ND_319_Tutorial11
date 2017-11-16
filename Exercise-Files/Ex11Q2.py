81e199834379c0968ac3b1fcb773f1cef2858e3c
Michelle Corley <mcorley1@nd.edu>
2017-11-16 13:40:33 -0500

    managed to get successful output of .fasta files, check and see if its working for you guys

--- Exercise-Files/Ex11Q2.py
+++ Exercise-Files/Ex11Q2.py
@@ -2,25 +2,26 @@ import numpy
 import pandas
 import re
 
-infile="motifsort.fasta"
-out1="motif1.fasta"
-out2="motif2.fasta"
-null="null.fasta"
+# open files to read and write
+infile=open("motifsort.fasta","r")
+out1=open("motif1.fasta","w")
+out2=open("motif2.fasta","w")
+null=open("null.fasta","w")
 
-#define the patterns
-Pat1="AKKPRVZE"
-Pat2="AAQWWRNYGG"
+#define the REGEX patterns
+Pat1=r"AKKPRVZE"
+Pat2=r"AAQWWRNYGG"
 
 for line in infile:
     line=line.strip()
     if line[0] == ">":
         seqid=line
     else:
-        if Pat1 in in line:
+        if Pat1 in line:
         #(re.search(Pat1, line))
             out1.write(seqid + "\n")
             out1.write(line + "\n")
-        elif if Pat2 in line: 
+        elif Pat2 in line: 
         #(re.search(Pat2, line))
             out2.write(seqid + "\n")
             out2.write(line + "\n")
